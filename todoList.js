7be90fb876b9de22af963c806283840d3a883058
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-02-07 22:41:28 +0100

    added a variety of other methods

--- todoList.js
+++ todoList.js
@@ -107,25 +107,33 @@ class TodoList {
     });
     return newTodoList;
   }
+
+  findByTitle(title) {
+    return this.filter((todo) => todo.getTitle() === title).first();
+  }
+
+  allDone() {
+    return this.filter((todo) => todo.isDone());
+  }
+
+  allNotDone() {
+    return this.filter((todo) => !todo.isDone());
+  }
+
+  markDone(title) {
+    const todo = this.findByTitle(title);
+    if (todo) todo.markDone();
+  }
+
+  markAllDone() {
+    this.forEach((todo) => todo.markDone());
+  }
+
+  markAllUndone() {
+    this.forEach((todo) => todo.markUndone());
+  }
+
+  toArray() {
+    return [...this.todos];
+  }
 }
-
-let todo1 = new Todo("Buy milk");
-let todo2 = new Todo("Clean room");
-let todo3 = new Todo("Go to the gym");
-let todo4 = new Todo("Go shopping");
-let todo5 = new Todo("Feed the cats");
-let todo6 = new Todo("Study for Launch School");
-let list = new TodoList("Today's Todos");
-
-list.add(todo1);
-list.add(todo2);
-list.add(todo3);
-list.add(todo4);
-list.add(todo5);
-list.add(todo6);
-todo1.markDone();
-todo5.markDone();
-
-let doneTodos = list.filter((todo) => todo.isDone());
-console.log(doneTodos);
-console.log(list.filter((todo) => todo.isDone()).first());
