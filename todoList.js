d1d0226538c5e775686147d7279c1da9dcfaec52
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-02-10 22:33:36 +0100

    removed todo to a separate class and added exports to perform testing

--- todoList.js
+++ todoList.js
@@ -1,33 +1,4 @@
-class Todo {
-  static DONE_MARKER = "X";
-  static UNDONE_MARKER = " ";
-
-  constructor(title) {
-    this.title = title;
-    this.done = false;
-  }
-
-  toString() {
-    let marker = this.isDone() ? Todo.DONE_MARKER : Todo.UNDONE_MARKER;
-    return `[${marker}] ${this.title}`;
-  }
-
-  markDone() {
-    this.done = true;
-  }
-
-  markUndone() {
-    this.done = false;
-  }
-
-  isDone() {
-    return this.done;
-  }
-
-  getTitle() {
-    return this.title;
-  }
-}
+const Todo = require("./todo");
 
 class TodoList {
   constructor(title) {
@@ -137,3 +108,5 @@ class TodoList {
     return [...this.todos];
   }
 }
+
+module.exports = TodoList;
